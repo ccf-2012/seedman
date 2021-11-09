19470d060ea57d13648c78b50377819113ed02d3
ccf2012 <crlf66@163.com>
2021-11-09 21:17:17 +0800

    db option

--- seedman/settings.py
+++ seedman/settings.py
@@ -89,6 +89,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR /  'db/db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,
+        }
     }
 }
 
