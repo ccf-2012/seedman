142b78c019bd8be10f65244b76faa4d741e81c71
ccf2012 <crlf66@163.com>
2021-11-09 21:13:16 +0800

    tweak

--- seedclient/forms.py
+++ seedclient/forms.py
@@ -11,6 +11,7 @@ class SeedClientForm(forms.ModelForm):
     #     error_messages={'required': '请输入新密码。'})
 
     # password = forms.CharField(widget=forms.PasswordInput)
+    root_dir = forms.CharField(label='存储根目录', required=False)
     class Meta:
         model = SeedClientSetting
         fields = ['clienttype', 'name', 'host', 'port', 'username', 'password', 'root_dir']
--- seedman/settings.py
+++ seedman/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@r8#a$%f!pe*$fo47-l51y+39ypv-f+jfthi*ml(=vfe@sa0hk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'seedman.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR /  'db/db.sqlite3',
     }
 }
 
