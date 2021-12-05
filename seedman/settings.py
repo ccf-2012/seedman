9394e99bc339e2c72b6fb57e087c5039ee6a2fab
ccf2012 <87898914+ccf-2012@users.noreply.github.com>
2021-12-05 11:57:30 +0800

    Update settings.py

--- seedman/settings.py
+++ seedman/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@r8#a$%f!pe*$fo47-l51y+39ypv-f+jfthi*ml(=vfe@sa0hk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
